d990e7df582b0174f102ce39837ad2c46967e70f
Leonardo Matos <leomp120894@gmail.com>
2018-01-09 15:56:02 -0200

    init app

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict'
+
+/**
+ * @file E-Com Plus service for customer login and signup with Passportjs
+ * @copyright E-Com Club. All rights reserved. Since 2016
+ * <br>E-COM CLUB SOFTWARES PARA E-COMMERCE LTDA / CNPJ: 24.356.660/0001-78
+ * @license MIT
+ * @author E-Com Club
+ */
+
+// NodeJS filesystem module
+const fs = require('fs')
+
+function error (err) {
+  // fatal error
+  // log to file before exit
+  let msg = '\n[' + new Date().toString() + ']\n'
+  if (err) {
+    if (err.hasOwnProperty('stack')) {
+      msg += err.stack
+    } else if (err.hasOwnProperty('message')) {
+      msg += err.message
+    } else {
+      msg += err.toString()
+    }
+    msg += '\n'
+  }
+
+  fs.appendFile('/var/log/nodejs/_stderr', msg, () => {
+    process.exit(1)
+  })
+}
+
+process.on('uncaughtException', error)
+
+// web application
+// recieve requests from Nginx by reverse proxy
+const web = require('./bin/web.js')
+fs.readFile('file', 'utf8', (err, data) => {
+  if (err) {
+    // can't read config file
+    error(err)
+  } else {
+    let config
+    try {
+      config = JSON.parse(data)
+      // start web app
+      web(config)
+    } catch (e) {
+      // invalid JSON
+      error(new Error('config/config.json must contain valid JSON'))
+    }
+  }
+})
+
+// local application
+// executable server side only
+require('./bin/local.js')
