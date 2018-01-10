a6ba4b88111d8e9a657dd7393ed9c26add352dc6
Leonardo Matos <leomp120894@gmail.com>
2018-01-10 13:02:17 -0200

    work with cookies

--- bin/web.js
+++ bin/web.js
@@ -20,8 +20,11 @@ const Strategies = {
   }
 }
 
+// process.cwd() can change
+// keep initial absolute path
+let root = process.cwd()
 // read config file
-fs.readFile(process.cwd() + '/config/config.json', 'utf8', (err, data) => {
+fs.readFile(root + '/config/config.json', 'utf8', (err, data) => {
   if (err) {
     // can't read config file
     throw err
@@ -38,8 +41,16 @@ fs.readFile(process.cwd() + '/config/config.json', 'utf8', (err, data) => {
       })
     })
 
-    // initialize Passport
-    app.use(passport.initialize())
+    // keep id and token on cookies
+    let cookieOptions = {
+      // browser session only
+      'expires': 0,
+      // cookie only accessible by the web server
+      'httpOnly': true,
+      // cookie should be signed
+      // can detect if the client modified the cookie
+      'signed': true
+    }
 
     // initialize OAuth strategies
     let strategies = config.strategies
@@ -64,16 +75,56 @@ fs.readFile(process.cwd() + '/config/config.json', 'utf8', (err, data) => {
             }
           }
 
+          let path = config.baseUri + provider
           // callback always with the same pattern
-          options.callbackURL = config.host + config.baseUri + provider + '/callback.json'
+          options.callbackURL = config.host + path + '/callback.html'
 
           // add strategy
           passport.use(new Strategy.Init(options, (accessToken, refreshToken, profile, done) => {
-            return done(null, profile)
+            // find or create user account
+            // generate JSON Web Token
+            let user = profile
+            user.token = 'JWT'
+            return done(null, user)
           }))
+
+          app.get(path + '/:id', (req, res, next) => {
+            // check id
+            if (/^[\w.]{32}$/.test(req.params.id)) {
+              // create id cookie
+              res.cookie('_passport_id', req.params.id, cookieOptions)
+              // pass next middleware
+              // run passport
+              next()
+            } else {
+              res.sendStatus(400)
+              res.send('Invalid ID, must follow RegEx pattern ^[\\w.]{32}$')
+            }
+          }, passport.authenticate(provider, {
+            session: false
+          }))
+
+          app.get(path + '/callback.html', passport.authenticate(provider, {
+            session: false
+          }), (req, res) => {
+            let token
+            if (typeof req.user === 'object' && req.user !== null && req.user.token) {
+              // successful authentication
+              token = req.user.token
+            } else {
+              token = null
+            }
+            // create token cookie
+            res.cookie('_passport_token', token, cookieOptions)
+            // return HTML file
+            res.sendFile(root + '/assets/callback.html')
+          })
         }
       }
     }
+
+    // initialize Passport
+    app.use(passport.initialize())
 
     app.listen(config.proxyPort, () => {
       logger.log('Running Express server on port ' + config.proxyPort)
