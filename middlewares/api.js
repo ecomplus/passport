f42d95698018334e41eb72adc6fba8e607584955
Leonardo Matos <leomp120894@gmail.com>
2018-04-18 10:31:09 -0300

    abstract requests to other resources

--- middlewares/api.js
+++ middlewares/api.js
@@ -2,4 +2,14 @@
 
 module.exports = (req, res, next) => {
   // treat API endpoints
+  switch (req.params.resource) {
+    case 'me':
+      // customer resource
+      break
+    case 'cart':
+    case 'order':
+      break
+    default:
+      next()
+  }
 }
